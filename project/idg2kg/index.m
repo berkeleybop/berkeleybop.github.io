77be7af230c6884623af9e26526c2cb620eb5659
Nomi Harris <nlharris@users.noreply.github.com>
2020-02-19 14:21:28 -0800

    IDG2KG page first draft
    What should the homepage link be?

---
+++ project/idg2kg/index.m
@@ -0,0 +1,10 @@
+---
+layout: project
+title: IDG2KG
+active: yes
+funding: NIH U01 CA239108-01
+homepage: 
+---
+
+"Illuminating the Druggable Genome by Knowledge Graphs" (IDG2KG), funded by the NCATS as part of the [Illuminating the Druggable Genome](https://ncats.nih.gov/idg) effort, builds on leading computational resources in biomedicine (including the Monarch Initiative, which is co-led by BBOP PI Chris Mungall) to create a knowledgebase about drugs, proteins and diseases.
+We use machine learning (ML) approaches to learn disease-protein associations, paving the way to forming hypotheses about how to treat diseases.Prioritizing novel drug targets is a slow, high-risk process; the IDK2KG project aims to accelerate this critical step in drug development.
